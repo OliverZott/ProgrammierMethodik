321e2de5316013bd8cdb717849606460729c4ed5
Oliver Zott <zott_oliver@web.de>
2020-03-29 14:44:00 +0200

    comments added for git testing

--- Programmiermethodik/src/basics/HelloWorldApplication.java
+++ Programmiermethodik/src/basics/HelloWorldApplication.java
@@ -1,9 +1,22 @@
 package basics;
 
-public class HelloWorldApplication {
+/**
+ * Class implements a simple hello world example.
+ * source: Programmiermethodik VO
+ * 
+ * @author Dura
+ *
+ */
 
+public class HelloWorldApplication {
+	
+	/**
+	 * Main method for HelloWorld programm.
+	 * 
+	 * @param arg
+	 * 			not used
+	 */
 	public static void main(String[] args) {
-		
 		System.out.println("Hello World!");
 	}
 }
