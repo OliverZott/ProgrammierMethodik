3a79a8cd51318949ed97b1b335377cb693de8977
Oliver Zott <zott_oliver@web.de>
2020-04-01 19:46:25 +0200

    add ex2

---
+++ Programmiermethodik/src/basics/Basics02_Foreach.java
@@ -0,0 +1,14 @@
+package basics;
+
+public class Basics02_Foreach {
+	
+	public static void main(String[] args) {
+		
+		int[] intArray = {1,2,3};
+		
+		for(int argument : intArray) {
+			System.out.println(argument);
+		}
+	}
+
+}
